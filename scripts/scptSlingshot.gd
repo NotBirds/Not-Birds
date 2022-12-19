1d6f12b6a4928aaff1d0bbe95f907e7d7684dc0d
Lautaro Bernabé Otaño <otanolautarob@gmail.com>
2022-12-19 18:59:23 -0300

    Code refactoring

--- scripts/scptSlingshot.gd
+++ scripts/scptSlingshot.gd
@@ -3,38 +3,40 @@ var can_be_used = false;
 var mouse_pos
 onready var top_sling = $lTop_sling
 onready var down_sling = $sTexture/lDown_sling
-onready var sling_center = $pCenter
-onready var not_bird = $asNot_bird
 onready var bird = preload("res://entities/scnBird.tscn")
+
+export(float) var speed_modifier
+export(float) var max_radius
 
 func _ready():
 	top_sling.hide()
 	down_sling.hide()
-	sling_center = sling_center.position #if the sling is going to move, remember to update this poss
-	
-
-func _process(_delta):
-	if !can_be_used:
-		return
 
 func _input(_event):
 	if !can_be_used:
 		return
 	if Input.is_action_pressed("leftMouse"):
 			mouse_pos = get_local_mouse_position()
-			top_sling.show()
-			down_sling.show()
-			if mouse_pos.distance_to(sling_center) > 1000:
-				mouse_pos = ((mouse_pos - sling_center).normalized() * 1000  + sling_center)
+			show_sling(true)
+			# Limit the distance radius
+			if mouse_pos.distance_to($pCenter.position) > max_radius:
+				mouse_pos = ((mouse_pos - $pCenter.position).normalized() * max_radius + $pCenter.position)
 			top_sling.points[0] = mouse_pos
 			down_sling.points[0] = mouse_pos
-			not_bird.position += (mouse_pos - not_bird.position) * Vector2(0.5,0.5)
+			$asNot_bird.position = mouse_pos
 	elif Input.is_action_just_released("leftMouse"):
+		show_sling(false)
+		throw_not_bird($asNot_bird.global_position)
+		$asNot_bird.position = Vector2(0, 0)
+		can_be_used = false
+
+func show_sling(show):
+	if show:
+		top_sling.show()
+		down_sling.show()
+	else:
 		top_sling.hide()
 		down_sling.hide()
-		throw_not_bird(not_bird.global_position)
-		not_bird.position = Vector2(-580 , 271) #+= (Vector2(-580 , 271) - not_bird.position) * Vector2(0.5,0.5) #makes it go smoot 
-		can_be_used = false
 
 func _on_aSlingshot_Hitbox_input_event(_viewport, _event, _shape_idx):#this strigger when the mouse is over the chHitbox
 	can_be_used = true
@@ -44,5 +46,5 @@ func throw_not_bird(last_mouse_pos):
 	get_parent().add_child(real_bird)
 	real_bird.position = last_mouse_pos
 	var speed = $pCenter.global_position - last_mouse_pos
-	real_bird.apply_impulse(Vector2(), speed * 2)
-	not_bird.hide()
+	real_bird.apply_central_impulse(speed * speed_modifier)
+	$asNot_bird.hide()
