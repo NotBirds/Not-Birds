5d60c00797b37293ca53b2b38fb0ea9f846235c2
Lautaro Bernabé Otaño <otanolautarob@gmail.com>
2022-12-28 18:54:52 -0300

    Add main menu, sounds and part of level 1

--- scripts/scptCamera.gd
+++ scripts/scptCamera.gd
@@ -1,18 +1,4 @@
 extends Camera2D
-var can_be_used = true
-const DEAD_ZONE = 80
-var camera_speed
 	
 func _input(event: InputEvent) -> void:
-	if event is InputEventMouseButton:
-		if event.button_index == BUTTON_LEFT:
-			can_be_used = !event.pressed
-			
-	if event is InputEventMouseMotion || event is InputEventMouseButton:
-		var _target = event.global_position - get_viewport().size * 0.1
-		if _target.length() < DEAD_ZONE:
-			self.position = Vector2(0,0)
-		else:
-			var move_to = _target.normalized() * (_target.length() - DEAD_ZONE) * 0.1
-			self.position = self.position + (move_to - self.position) * 0.5
-			
+	pass
--- scripts/scptSlingshot.gd
+++ scripts/scptSlingshot.gd
@@ -2,46 +2,44 @@ extends Node2D
 var can_be_used = false;
 var mouse_pos
 onready var top_sling = $lTop_sling
-onready var down_sling = $sTexture/lDown_sling
+onready var down_sling = $lDown_sling
+onready var aim_sound = $aimSound
+onready var shot_sound = $shotSound
 onready var bird = preload("res://scenes/entities/scnBird.tscn")
 
 export(float) var speed_modifier
 export(float) var max_radius
 
-func _ready():
-	top_sling.hide()
-	down_sling.hide()
-
 func _input(_event):
-	if !can_be_used:
-		return
-	if Input.is_action_pressed("leftMouse"):
+	if can_be_used:
+		if Input.is_action_pressed("leftMouse"):
+			if !aim_sound.playing:
+				aim_sound.play()
 			mouse_pos = get_local_mouse_position()
-			show_sling(true)
+			set_sling_visible(true)
 			# Limit the distance radius
 			if mouse_pos.distance_to($pCenter.position) > max_radius:
 				mouse_pos = ((mouse_pos - $pCenter.position).normalized() * max_radius + $pCenter.position)
 			top_sling.points[0] = mouse_pos
 			down_sling.points[0] = mouse_pos
 			$asNot_bird.position = mouse_pos
-	elif Input.is_action_just_released("leftMouse"):
-		show_sling(false)
-		throw_not_bird($asNot_bird.global_position)
-		$asNot_bird.position = Vector2(0, 0)
-		can_be_used = false
+		elif Input.is_action_just_released("leftMouse"):
+			aim_sound.stop()
+			throw_not_bird($asNot_bird.global_position)
 
-func show_sling(show):
-	if show:
-		top_sling.show()
-		down_sling.show()
-	else:
-		top_sling.hide()
-		down_sling.hide()
-
-func _on_aSlingshot_Hitbox_input_event(_viewport, _event, _shape_idx):#this strigger when the mouse is over the chHitbox
+func set_sling_visible(show):
+	top_sling.visible = show;
+	down_sling.visible = show;
+	
+# This triggers when the mouse is over the hitbox
+func _on_aSlingshot_Hitbox_input_event(_viewport, _event, _shape_idx):
 	can_be_used = true
 
 func throw_not_bird(last_mouse_pos):
+	shot_sound.play()
+	set_sling_visible(false)
+	$asNot_bird.position = Vector2(0, 0)
+	can_be_used = false
 	var real_bird = bird.instance()
 	get_parent().add_child(real_bird)
 	real_bird.position = last_mouse_pos
