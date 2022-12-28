50cea1d4bd67e5be7b19b6cd65822a570aaba91d
Lautaro Bernabé Otaño <otanolautarob@gmail.com>
2022-12-28 16:39:08 -0300

    Turn background and foreground into scenes

--- scripts/scptCamera.gd
+++ scripts/scptCamera.gd
@@ -4,18 +4,15 @@ const DEAD_ZONE = 80
 var camera_speed
 	
 func _input(event: InputEvent) -> void:
-	if !can_be_used:
-		camera_speed = 0.013
-	if event is InputEventMouseMotion:
-		camera_speed =+ 0.02
-		var _target = event.position - get_viewport().size * camera_speed
+	if event is InputEventMouseButton:
+		if event.button_index == BUTTON_LEFT:
+			can_be_used = !event.pressed
+			
+	if event is InputEventMouseMotion || event is InputEventMouseButton:
+		var _target = event.global_position - get_viewport().size * 0.1
 		if _target.length() < DEAD_ZONE:
 			self.position = Vector2(0,0)
 		else:
-			self.position = _target.normalized() * (_target.length() - DEAD_ZONE) * camera_speed
-
-func _on_aLoock_mouse_entered():
-	can_be_used = false
-
-func _on_aLoock_mouse_exited():
-	can_be_used = true
+			var move_to = _target.normalized() * (_target.length() - DEAD_ZONE) * 0.1
+			self.position = self.position + (move_to - self.position) * 0.5
+			
