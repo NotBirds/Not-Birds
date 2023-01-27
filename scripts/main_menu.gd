edf652ae82645512209dd821ba09f7fa43eaa8e4
Lautaro Bernabé Otaño <otanolautarob@gmail.com>
2023-01-27 16:53:55 -0300

    Make levesl inherited

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -3,7 +3,7 @@ extends Control
 onready var config_overlay = preload("res://scenes/ui/config_overlay.tscn")
 
 func _on_start_game_pressed():
-	get_tree().change_scene("res://scenes/levels/template_proposal.tscn")
+	get_tree().change_scene("res://scenes/levels/level1.tscn")
 
 
 func _on_configuration_pressed():
